bcb0b8ade957a731fa066ded4c05035fc9f4e97d
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-09-11 02:14:09 -0300

    Trabajo Practico N2

---
+++ TrabajoPractico1/Entidades/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+       private static string ValidarOperador(char operador)
+       {     
+
+            if(operador == '+' || operador == '-' || operador == '/' || operador == '*')
+            {
+                return operador.ToString();
+            }
+
+            return "+";
+       }
+
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+
+            if(operador == "+")
+            {
+                return num1 + num2;
+            }
+            else if( operador == "-")
+            {
+                return num1 - num2;
+            }
+            else if(operador == "/")
+            {
+                return num1 / num2;
+
+            }
+            else if(operador == "*")
+            {
+                return num1 * num2;
+            }
+
+        
+        }
+
+    }
+}
