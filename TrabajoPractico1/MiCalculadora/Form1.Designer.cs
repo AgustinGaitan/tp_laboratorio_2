37c61d40738858e292ea21f603d9685888005f51
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-09-13 04:58:26 -0300

    Tp terminado

--- TrabajoPractico1/MiCalculadora/Form1.Designer.cs
+++ TrabajoPractico1/MiCalculadora/Form1.Designer.cs
@@ -137,6 +137,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 421);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.cmbOperador);
