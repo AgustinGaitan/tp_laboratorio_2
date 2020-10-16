ee74ef01913f0ce696ac59a7468cf2f0e768e580
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-10-16 02:39:53 -0300

    Trabjao Practico N3 empezando

---
+++ Gaitan.Agustin.2A.TP3/Entidades/Persona.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private string nacionalidad;
+        private long dni;
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return this.apellido; }
+            set { this.apellido = value; }
+
+        }
+
+        public string Nacionalidad
+        {
+            get 
+            {
+                return this.nacionalidad;
+            }
+            set 
+            { 
+                
+
+            }
+
+        }
+
+        public long Dni
+        {
+            get
+            {
+                return this.dni;
+            }
+            set
+            {
+                
+
+                if((value > 0 && value < 90000000) && (this.Nacionalidad == "argentino"))
+                {
+                    this.dni = value;
+                }
+                else if((value >= 90000000 && value < 100000000) && (this.Nacionalidad == "extranjero"))
+                {
+                    this.dni = value;
+                }
+                else
+                {
+                    throw 
+                }
+
+            }
+
+        }
+
+
+        public Persona(string nombre, string apellido, string nacionalidad, int dni)
+        {
+
+        }
+    }
+
+}
