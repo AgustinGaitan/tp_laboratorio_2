afce5130774a04014305f2bf74250f5a53fef4a0
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-10-25 20:52:53 -0300

    TP3 DNi validado

--- Gaitan.Agustin.2A.TP3/ClasesAbstractas/Persona.cs
+++ Gaitan.Agustin.2A.TP3/ClasesAbstractas/Persona.cs
@@ -14,6 +14,12 @@ namespace ClasesAbstractas
         private string nacionalidad;
         private long dni;
 
+
+        public enum ENacionalidad
+        {
+            Argentino,Extranjero
+        }
+
         public string Nombre
         {
             get { return this.nombre; }
@@ -59,7 +65,7 @@ namespace ClasesAbstractas
 
         }
 
-        public long Dni
+        public long DNI
         {
             get
             {
@@ -86,10 +92,71 @@ namespace ClasesAbstractas
 
         }
 
+        public string StringToDNI
+        {
+            set
+            { 
 
-        public Persona(string nombre, string apellido, string nacionalidad, int dni)
+            }
+        }
+
+
+
+        public Persona()
         {
 
+        }
+
+        public Persona(string nombre, string apellido,ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+
+            return this.ValidarDni(nacionalidad, Convert.ToInt32(dato));
+
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            int dniAInt = Convert.ToInt32(dato);
+            int retorno = -1;
+ 
+           if((dato.Length >= 0 && dato.Length <= 8)  && (!(dato.Any(char.IsLetter) && dato.Any(char.IsSymbol)))) 
+           {    
+                if((nacionalidad == ENacionalidad.Argentino && dniAInt >= 1 && dniAInt <= 89999999) ||
+                    nacionalidad == ENacionalidad.Extranjero && dniAInt >= 90000000 && dniAInt <= 99999999)
+                {
+
+                    retorno = dniAInt;
+                        
+                }
+                else
+                {
+                    throw new NacionalidadInvalidaException("Error.Nacionalidad inválida.");
+                }
+           
+           }
+           else
+           {    
+
+                throw new DniInvalidoException("Error. Dni inválido.");
+                
+           }
+
+            return retorno;
         }
     }
 
