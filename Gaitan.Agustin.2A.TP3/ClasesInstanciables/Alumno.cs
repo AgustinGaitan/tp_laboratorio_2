13c11b8ad9617eaab45c3dd600f9ecd2d2f08ac0
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-10-26 04:11:13 -0300

    Clase Alumno hecha. Proxima a hacer: Universidad

---
+++ Gaitan.Agustin.2A.TP3/ClasesInstanciables/Alumno.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesAbstractas;
+
+namespace ClasesInstanciables
+{
+    public sealed class Alumno : Universitario
+    {
+
+       private Universidad.EClases claseQueToma;
+        private EEstadoCuenta estadoCuenta;
+
+        public enum EEstadoCuenta
+        {
+            AlDia, Deudor, Becado
+        }
+
+        public Alumno()
+            :base()
+        {
+
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Universidad.EClases claseQueToma)
+            :base(id,nombre,apellido,dni,nacionalidad)
+        {
+            this.claseQueToma = claseQueToma;
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Universidad.EClases claseQueToma, EEstadoCuenta estadoCuenta)
+            :this(id,nombre,apellido,dni,nacionalidad, claseQueToma)
+        {
+            this.estadoCuenta = estadoCuenta;
+        }
+
+        protected override string ParticiparEnClase()
+        {
+            return "TOMA CLASE DE " + this.claseQueToma;
+        }
+        protected override string MostrarDatos()
+        {
+            return base.MostrarDatos() + this.ParticiparEnClase();
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        public static bool operator ==(Alumno a, Universidad.EClases clase)
+        {
+            return (a.estadoCuenta != EEstadoCuenta.Deudor && a.claseQueToma == clase);
+        }
+
+        public static bool operator !=(Alumno a, Universidad.EClases clase)
+        {
+            return !(a == clase);
+        }
+    }
+}
