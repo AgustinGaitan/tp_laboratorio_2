c847d83e15e4c95ffd90a07944ad792040317b22
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-11-01 20:35:05 -0300

    TP3 TERMINADO 100%. AGREGADO CONSOLE.TITLE

--- Gaitan.Agustin.2A.TP3/TestConsole/Program.cs
+++ Gaitan.Agustin.2A.TP3/TestConsole/Program.cs
@@ -14,6 +14,7 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            Console.Title = "Agustin Gaitan 2A TP3";
 
             Universidad uni = new Universidad();
 
