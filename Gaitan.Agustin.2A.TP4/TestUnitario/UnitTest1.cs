62d75702fe26418778232b6c089237dc4a09cae2
AgustinGaitan <agusgaitan2009@hotmail.com>
2020-11-15 21:27:05 -0300

    TP4

--- Gaitan.Agustin.2A.TP4/TestUnitario/UnitTest1.cs
+++ Gaitan.Agustin.2A.TP4/TestUnitario/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using Excepciones;
 
 namespace TestUnitario
 {
@@ -7,8 +9,45 @@ namespace TestUnitario
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_LongitudInvalida()
         {
+            try
+            {
+                Barra barraUno = new Barra(500, 600, 1);
+            }
+            catch(Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(LongitudInvalidaException));
+            }
+            
+        }
+
+        [TestMethod]
+        public void Test_PesoInvalido()
+        {
+            try
+            {
+                Mancuerna barraUno = new Mancuerna(500, 400, 1);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(PesoInvalidoException));
+            }
+
+        }
+
+        [TestMethod]
+        public void Test_ElementosMaxInvalido()
+        {
+            try
+            {
+                ElementosGimnasio barraUno = new ElementosGimnasio(100, 5, 6, 1);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(CantMaxElementosInvalidaException));
+            }
+
         }
     }
 }
